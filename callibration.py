39fca2ab8334a02876667003b39f6c9bcd0ddcf3
Vansh Uppal <vanshuppal2002@gmail.com>
2022-03-12 02:09:04 +0530

    While loop instead of for to ensure proper execution

--- callibration.py
+++ callibration.py
@@ -9,10 +9,15 @@ hands=mpHands.Hands(False)
 mpDraw=mp.solutions.drawing_utils
 pTime=0
 cTime=0
-for j in range(40):
-        success,img=cap.read()
-        imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        results=hands.process(imgRGB)
+temp=40
+while temp>=0:
+    success,img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+    if results.multi_hand_landmarks:
+        temp-=1
         lx8=[]
         lx0=[]
         lx5=[]
