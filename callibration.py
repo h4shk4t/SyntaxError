f0622a3d7f63bab5c79772efc6b8cfc6ca496563
Vansh Uppal <vanshuppal2002@gmail.com>
2022-03-12 20:02:40 +0530

    should identify all fingers, i removed the function to work thodha sa on this so add that

--- callibration.py
+++ callibration.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import mediapipe as mp
 import math
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(1)
 print("Keep your hands out upright and move them")
 mpHands=mp.solutions.hands
 hands=mpHands.Hands(False)
@@ -51,7 +51,6 @@ while temp>=0:
                     ly8.append(yList[8])
                     ly0.append(yList[0])
                     ly5.append(yList[5])
-                    #list
                     x8=(sum(lx8))/40
                     x0=(sum(lx0))/40
                     x5=(sum(lx5))/40
@@ -65,7 +64,7 @@ while temp>=0:
                     y16=(sum(ly16))/40
                     y20=(sum(ly20))/40
                     ind=math.hypot(x8-x0,y8-y0)
-                    mid=math.hypot(x12-x0,y8-y0)
+                    mid=math.hypot(x12-x0,y12-y0)
                     ring=math.hypot(x16-x0,y16-y0)
                     pink=math.hypot(x20-x0,y20-y0)
                     palm=math.hypot(x5-x0,y5-y0)
@@ -96,14 +95,53 @@ while True:
             x20=xList[20]
             y20=yList[20]
             #index finger
-            print(math.hypot(x8-x0,y8-y0)/math.hypot(x5-x0,y5-y0))
+            '''     print(math.hypot(x8-x0,y8-y0)/math.hypot(x5-x0,y5-y0))
             print("Index Finger is: ")
             if math.hypot(x8-x0,y8-y0)/math.hypot(x5-x0,y5-y0)>(round(ind/palm,1)-0.1):
                 print("Raised")
+                iN=2
             elif (math.hypot(x8-x0,y8-y0)/math.hypot(x5-x0,y5-y0))>(3*(round(ind/palm,1))/4):
                 print("Half")
+                iN=1
             else:
                 print("Down")
+                iN=0'''
+            #middle finger
+            print(math.hypot(x8-x0,y8-y0)/math.hypot(x5-x0,y5-y0))
+            print("Middle Finger is: ")
+            if math.hypot(x12-x0,y12-y0)/math.hypot(x5-x0,y5-y0)>(round(mid/palm,1)-0.1):
+                print("Raised")
+                mN=2
+            elif (math.hypot(x12-x0,y12-y0)/math.hypot(x5-x0,y5-y0))>(3*(round(mid/palm,1))/4):
+                print("Half")
+                mN=1
+            else:
+                print("Down")
+                mN=0
+            #ring finger
+            print(math.hypot(x16-x0,y16-y0)/math.hypot(x5-x0,y5-y0))
+            print("Ring Finger is: ")
+            if math.hypot(x16-x0,y16-y0)/math.hypot(x5-x0,y5-y0)>(round(ring/palm,1)-0.1):
+                print("Raised")
+                rN=2
+            elif (math.hypot(x16-x0,y16-y0)/math.hypot(x5-x0,y5-y0))>(3*(round(ring/palm,1))/4):
+                print("Half")
+                rN=1
+            else:
+                print("Down")
+                rN=0
+            #pinky finger
+            print(math.hypot(x20-x0,y20-y0)/math.hypot(x5-x0,y5-y0))
+            print("Pinky Finger is: ")
+            if math.hypot(x20-x0,y20-y0)/math.hypot(x5-x0,y5-y0)>(round(pink/palm,1)-0.05):
+                print("Raised")
+                pN=2
+            elif (math.hypot(x20-x0,y20-y0)/math.hypot(x5-x0,y5-y0))>(3*(round(pink/palm,1))/4):
+                print("Half")
+                pN=1
+            else:
+                print("Down")
+                pN=0
             mpDraw.draw_landmarks(img,i,mpHands.HAND_CONNECTIONS)
     cTime=time.time()
     fps=1/(cTime-pTime)
